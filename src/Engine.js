e355be0af435978a32090f0abd6d18f24198e9a9
Julien SMAGUE <julien.smague@laposte.net>
2015-11-12 11:53:48 +0100

    Ajout de l'histoire 2

--- src/Engine.js
+++ src/Engine.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var Engine = function () {
-
+    var joueur1,joueur2;
     var plateau = new Array(36);
 
     for(var i =0;i<6;i++ ){
@@ -10,6 +10,7 @@ var Engine = function () {
 
    this.initialisation = function(){
 
+
         plateau[0][0] = "black"; plateau[0][1] = "green"; plateau[0][2] = "white";plateau[0][3] = "blue";plateau[0][4] = "red";plateau[0][5] = "white";
         plateau[1][0] = "yellow";plateau[1][1] = "white";plateau[1][2] = "green";plateau[1][3] = "red";plateau[1][4] = "yellow";plateau[1][5] = "blue";
         plateau[2][0] = "blue";plateau[2][1] = "yellow";plateau[2][2] = "blue";plateau[2][3] = "white";plateau[2][4] = "black";plateau[2][5] = "red";
@@ -17,10 +18,14 @@ var Engine = function () {
         plateau[4][0] = "white";plateau[4][1] = "green";plateau[4][2] = "yellow";plateau[4][3] = "black";plateau[4][4] = "yellow";plateau[4][5] = "green";
         plateau[5][0] = "yellow";plateau[5][1] = "blue";plateau[5][2] = "black";plateau[5][3] = "red";plateau[5][4] = "green";plateau[5][5] = "black";
 
+   };
+
+    this.get_couleur = function(ligne,colonne) {
+            return plateau[ligne][colonne];
     };
 
-    this.get_position = function(ligne,colonne) {
-            return plateau[ligne][colonne];
+    this.set_couleur_joueur = function(joueur,couleur){
+        joueur = couleur;
     };
 
     this.test_juxstaposition = function(){
@@ -38,7 +43,19 @@ var Engine = function () {
             }
         }
 
-    }
+    };
+
+    this.test_couleur = function (couleur){
+
+        if(plateau[0][0] == couleur || plateau[0][5] == couleur || plateau[5][0] == couleur || plateau[5][5] == couleur ){
+
+            this.set_couleur_joueur(joueur1,couleur);
+            return couleur;
+
+        }else{
+            return false;
+        }
+    };
 };
 
 
--- test/test.js
+++ test/test.js
@@ -6,9 +6,16 @@ PalettoTestCase.prototype.testStory1 = function () {
     var e = new Engine();
     e.initialisation();
 
-    assertEquals("black",e.get_position(0,0));
-    assertEquals("white",e.get_position(0,5));
-    assertEquals("yellow",e.get_position(5,0));
+    assertEquals("black",e.get_couleur(0,0));
+    assertEquals("white",e.get_couleur(0,5));
+    assertEquals("yellow",e.get_couleur(5,0));
 
     assertEquals(true,e.test_juxstaposition());
 };
+
+PalettoTestCase.prototype.testStory2 = function(){
+    var e = new Engine();
+    var joueur1;
+    e.initialisation();
+    assertEquals("yellow", e.test_couleur("yellow"));
+}
